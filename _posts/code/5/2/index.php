ae155033c3856208ba8dc0a0fb3cd2e0a899c56e
Vves <vvesnet@gmail.com>
2022-03-22 21:29:42 +0200

    Ex 1 & 2

--- _posts/code/5/2/index.php
+++ _posts/code/5/2/index.php
@@ -2,13 +2,13 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Bolț</title>
+    <title>Title</title>
 </head>
 <body>
 
     <form action="bolt.php" method="POST">
-        <label for="num">Numar:</label><br>
-        <input type="text" name="number" id="num" required="required"><br>
+        <label for="array_num">Sir de numere:</label><br>
+        <input type="text" name="arrayNumbers" id="array_num" required="required"><br>
         <input type="submit">
     </form>
 
