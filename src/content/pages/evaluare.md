10c61d145d7319e10f9deb48ec87b97ff680de35
Victor <victorvlad19@gmail.com>
2024-06-26 09:33:11 +0300

    Evaluare

--- src/content/pages/evaluare.md
+++ src/content/pages/evaluare.md
@@ -5,6 +5,9 @@ description: "this is meta description"
 draft: false
 ---
 
+> - <a href="https://docs.google.com/spreadsheets/d/10K-Jmxh-eGZPkHtUCezMRnD9GVO_aImYjE8DF5JxgRM/edit?gid=0#gid=0" target="_blank">Planificare evaluare componenta S  </a>
+
+
 ### 🎯 Criterii de evaluare saptamana 8
 - HTML si CSS valide 
     -  <a href="https://validator.w3.org/#validate_by_input" target="_blank">Validare HTML</a>
--- src/layouts/partials/PostSidebar.tsx
+++ src/layouts/partials/PostSidebar.tsx
@@ -20,14 +20,25 @@ const PostSidebar = ({
         <div className="rounded bg-theme-light p-8 dark:bg-darkmode-theme-light">
             <h5 className="mb-6">Anunțuri</h5>
             <ul>
-                <li><FaRegDotCircle style={{ display: "inline-block"}}/> Studenţii din grupa <span style={{ fontWeight: "bolder"}}>A4</span> sunt rugați să vina sâmbătă de la 10:00 in laboratorul C411.</li>
+                <li><FaRegDotCircle style={{display: "inline-block"}}/>
+                    <a style={{textDecoration: "underline"}} href="https://docs.google.com/spreadsheets/d/10K-Jmxh-eGZPkHtUCezMRnD9GVO_aImYjE8DF5JxgRM/edit?usp=sharing"
+                       target="_blank" rel="noopener noreferrer"> Planificare evaluare componenta S </a>
+                </li>
                 <br/>
-            <li><FaRegDotCircle style={{ display: "inline-block"}}/> Studenţii din grupa <span style={{ fontWeight: "bolder"}}>B1</span> sunt rugați să vina sâmbătă de la 10:00 in laboratorul C411.</li>
+                <li><FaRegDotCircle style={{display: "inline-block"}}/> Studenţii din grupa <span
+                    style={{fontWeight: "bolder"}}>A4</span> sunt rugați să vina sâmbătă de la 10:00 in laboratorul
+                    C411.
+                </li>
+                <br/>
+                <li><FaRegDotCircle style={{display: "inline-block"}}/> Studenţii din grupa <span
+                    style={{fontWeight: "bolder"}}>B1</span> sunt rugați să vina sâmbătă de la 10:00 in laboratorul
+                    C411.
+                </li>
 
             </ul>
         </div>
       </div>
-      {/* <!-- tags --> */}
+        {/* <!-- tags --> */}
         <div className="mb-8">
             <div className="rounded bg-theme-light p-8 dark:bg-darkmode-theme-light">
                 <h5 className="mb-6">Timp Liber</h5>
